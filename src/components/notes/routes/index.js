b490bc91a620ee0909b3767aa2a34d83f1b534d1
Vinicius Silva Alves <vsa1410@hotmail.com>
2022-08-22 14:05:51 -0300

    Upload Files

--- src/components/notes/routes/index.js
+++ src/components/notes/routes/index.js
@@ -1,15 +1,19 @@
 import axios from "axios"
 import baseURL from "../../../services/api"
 
+function getToken(){
 
-let userId = localStorage.getItem("user")
+    let userId = localStorage.getItem("user")
     let id = JSON.parse(userId)
     let token=id.token
+    return token
+}
+
 const NotesService = {
 
     
 
     update:(id, params) => axios.put(`${baseURL}/notes/${id}`, params, {
-    headers:{ "x-access-token" : token}})
+    headers:{ "x-access-token" : getToken}})
 }
 export default NotesService
